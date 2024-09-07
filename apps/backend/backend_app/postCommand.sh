339e222a6a69b3182964146d9ea1b0bf96d59121
infinith4 <infinith4@gmail.com>
2024-09-07 17:04:12 +0900

    success

--- apps/backend/backend_app/postCommand.sh
+++ apps/backend/backend_app/postCommand.sh
@@ -1,3 +1,13 @@
+#### install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+#### install node
+. $HOME/.nvm/nvm.sh && \
+    nvm install --lts && \
+    nvm use --lts && \
+    node -v && npm -v && \
+    npm i -g yarn
+
 # npm i -g @nestjs/cli
 
 # nest new backend
--- apps/backend/init_sql.sh
+++ apps/backend/init_sql.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 . import-env mysql.env
 
-# line=$(head init_db/init_user.sql)
-# ret=${line/"$REPLACE_ME_MYSQL_PASSWORD"/"$MYSQL_PASSWORD"}
-# echo $ret > "./init_db/init_user.sql"
+line=$(head mysql/init.d/init_user.sql)
+ret=${line/"$REPLACE_ME_MYSQL_PASSWORD"/"$MYSQL_PASSWORD"}
+echo $ret > "./mysql/init.d/init_user.sql"
 
 
-FILE="./init_db/init_user.sql"
+# FILE="./init_db/init_user.sql"
 
-echo "----"
-echo $(cat $FILE | wc -l )
-count=1
-while true
-do
-  echo $count
-  if [ $count -eq $(cat $FILE | wc -l ) ]; then
-    exit 0
-  fi
-  count=`expr $count + 1`
-    # line="`cat $FILE | head -$i | tail -1`"
-    # echo "$line"
-    # $i=$i+1
-done
+# echo "----"
+# echo $(cat $FILE | wc -l )
+# count=1
+# while true
+# do
+#   echo $count
+#   if [ $count -eq $(cat $FILE | wc -l ) ]; then
+#     exit 0
+#   fi
+#   count=`expr $count + 1`
+#     # line="`cat $FILE | head -$i | tail -1`"
+#     # echo "$line"
+#     # $i=$i+1
+# done
