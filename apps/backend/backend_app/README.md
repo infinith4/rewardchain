5101de34d1c9af862921ef1c9cfd82d12804d02c
infinith4 <infinith4@gmail.com>
2024-09-07 22:37:55 +0900

    エラー解消

--- apps/backend/backend_app/README.md
+++ apps/backend/backend_app/README.md
@@ -11,6 +11,10 @@ user: prisma_user
 
 ## Backend
 
+cd backend
+
+yarn start:dev
+
 curl https://github.com/infinith4/rewardchain/apps/backend
 
 https://github.com/infinith4/rewardchain/apps/backend
@@ -20,7 +24,7 @@ curl https://github.com/infinith4/rewardchain
 yarn add -D prisma
 yarn add @prisma/client
 yarn add mysql2
-
+yarn add -D typescript ts-node @types/node
 
 CREATE USER 'prisma_user'@'%'
   IDENTIFIED BY 'rpannaNXks92jX';
@@ -46,3 +50,21 @@ rewardchain_mysql.sql をDBに反映し、 npx prisma db pull で以下のファ
 
 backend/prisma/schema.prisma
 
+
+
+vscode ➜ /backend_app/backend $ npx prisma generate
+Prisma schema loaded from prisma/schema.prisma
+Error: Prisma schema validation - (get-dmmf wasm)
+Error code: P1012
+error: Error parsing attribute "@relation": The argument `references` must refer to a unique criterion in the related model. Consider adding an `@unique` attribute to the field `task_id` in the model `disputations`.
+  -->  prisma/schema.prisma:83
+   | 
+82 |   task_items    task_items[]
+83 |   disputations  disputations    @relation(fields: [id], references: [task_id], onDelete: NoAction, onUpdate: NoAction, map: "fk_disputations_task_id_tasks_id")
+84 |   users         users           @relation(fields: [id], references: [id], onDelete: NoAction, onUpdate: NoAction, map: "fk_tasks_id_users_id")
+   | 
+
+Validation Error Count: 1
+[Context: getDmmf]
+
+Prisma CLI Version : 5.19.1
---
+++ apps/backend/backend_app/backend/prisma/seed.ts
@@ -0,0 +1,82 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // モデル投入用のデータ定義
+  const userData = [
+    {
+      id: 0,
+      user_type: 0,
+      first_name: 'Alice',
+      last_names: 'January',
+      username: 'alice',
+      email: 'infinith4@gmail.com',
+      hashed_password: 'password',
+    },
+    {
+      id: 1,
+      user_type: 1,
+      first_name: 'Bob',
+      last_names: 'February',
+      username: 'bob',
+      email: 'infinith4@gmail.com',
+      hashed_password: 'password',
+    },
+    {
+      id: 2,
+      user_type: 1,
+      first_name: 'Charlie',
+      last_names: 'March',
+      username: 'charlie',
+      email: 'infinith4@gmail.com',
+      hashed_password: 'password',
+    },
+  ]
+
+  for (const user of userData) {
+    await prisma.users.create({
+      data: user
+    });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+
+// const transfer = async () => {
+//     const users = [];
+//     for (const u of userData) {
+//         const user = prisma.users.create({
+//             data: u,
+//         })
+//         users.push(user);
+//     }
+//     return await prisma.$transaction(users);
+// }
+
+// // 定義されたデータを実際のモデルへ登録する処理
+// const main = async () => {
+//     console.log(`Start seeding ...`)
+
+//     await transfer();
+
+//     console.log(`Seeding finished.`)
+// }
+
+// // 処理開始
+// main()
+//     .catch((e) => {
+//         console.error(e)
+//         process.exit(1)
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect()
+//     })
