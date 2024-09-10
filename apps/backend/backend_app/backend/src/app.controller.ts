bc9119b609bf9505b78b9db58f05b3fca06a9547
infinith4 <infinith4@gmail.com>
2024-09-11 08:10:09 +0900

    Update app.controller.ts

--- apps/backend/backend_app/backend/src/app.controller.ts
+++ apps/backend/backend_app/backend/src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Put, Patch } from '@nestjs/common';
 import { AppService } from './app.service';
+import { debug } from 'console';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getRoot(): string {
     return this.appService.getHello();
+  }
+  @Get('/hello')
+  getHello(): {message: string} {
+    console.debug("get" + new Date().toISOString());
+    return {
+      message: this.appService.getHello()
+    }
+  }
+  @Post('/hello')
+  postHello(): {message: string} {
+    console.debug("post" + new Date().toISOString());
+    return {
+      message: this.appService.getHello()
+    }
+  }
+  @Put('/hello')
+  putHello(): {message: string} {
+    console.debug("put" + new Date().toISOString());
+    return {
+      message: this.appService.getHello()
+    }
+  }
+  @Patch('/hello')
+  patchHello(): {message: string} {
+    console.debug("patch" + new Date().toISOString());
+    return {
+      message: this.appService.getHello()
+    }
+  }
+  @Delete('/hello')
+  deleteHello(): {message: string} {
+    console.debug("delete" + new Date().toISOString());
+    return {
+      message: this.appService.getHello()
+    }
   }
 }
